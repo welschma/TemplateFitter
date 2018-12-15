bd250325f72270bb1dd3fd5b2d1a7affc2c2a584
Maximilian Welsch <maxwelsch93@gmail.com>
2018-12-15 10:53:20 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="TemplateFitter",
+    version="0.0.1",
+    author="Maximilian Welsch",
+    packages=[
+        "templatefitter"
+    ],
+    description="Perform binnend log-likelhood fits using histogram templates as pdfs.",
+)
