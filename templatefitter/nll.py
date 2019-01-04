fixed bug in computation of the nll value

templatefitter/nll.py
@@ -41,6 +41,9 @@ class PoissonNLL:
         values = self._templates.values
         return values/np.sum(values, axis=1).reshape(-1, 1)
 
+    @property
+    def x0(self):
+        return self._templates.yields
 
     def __call__(self, x):
         """This function is called by the minimize method.
@@ -53,8 +56,5 @@ class PoissonNLL:
             The value of the negative log likelihood at `x`.
         """
         poi = x
-        exp_evts_per_bin = np.matmul(poi, self.fraction_matrix())
-
-        return np.sum(exp_evts_per_bin - np.matmul(
-            np.log(exp_evts_per_bin), self._data.reshape(-1, 1))
-            )
+        exp_evts_per_bin = poi@self.fraction_matrix()
+        return np.sum(exp_evts_per_bin - self._data*np.log(exp_evts_per_bin))

tests/test_nll.py
@@ -56,9 +56,9 @@ class TestPoissonNLL(unittest.TestCase):
         np.testing.assert_array_equal(nll.fraction_matrix(), test_fractions)
         
         test_params = np.array([4,5])
-        test_evts_per_bin = np.matmul(test_params, test_fractions)
-        test_nll = np.sum(test_evts_per_bin - np.matmul(
-            np.log(test_evts_per_bin), hdata.reshape(-1,1))
-        ) 
+        test_evts_per_bin = test_params@test_fractions
+        test_nll = np.sum(
+            test_evts_per_bin - np.log(test_evts_per_bin)*self.hdata
+        )
         
         np.testing.assert_array_equal(nll(test_params), test_nll)