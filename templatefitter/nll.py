ce570c62154386306146d991b90447ad7f0aa472
Maximilian Welsch <maxwelsch93@gmail.com>
2019-01-05 16:17:28 +0100

    added abstract base class for negative log likelihood functions

--- templatefitter/nll.py
+++ templatefitter/nll.py
@@ -2,11 +2,25 @@
 functions which are used as const function to be minimized in
 the fit.
 """
+from abc import ABC, abstractmethod, abstractproperty
 
 import numpy as np
 
 
-class PoissonNLL:
+class AbstractNLL(ABC):
+    """Base class for all negative log likelihood functions.
+    """
+
+    @abstractproperty
+    def x0(self):
+        pass
+
+    @abstractmethod
+    def __call__(self):
+        pass
+
+
+class PoissonNLL(AbstractNLL):
     """A negative log likelihood (NLL) function for binned data using
     template histograms shapes as pdfs. The NLL is calculated as
     :math:`-\log(L) = \sum\limits_i^{n_{\mathrm{bins}}} \\nu_i - n_i \log(\\nu_i)`,
@@ -24,6 +38,12 @@ class PoissonNLL:
         A TemplateCollection instance. The templates are used to
         extract the contribution from each process described by 
         the templates to the measured data set.
+    
+    Attributes
+    ----------
+    x0 : np.ndarray
+        Expected yields for all used templates. Used as initial
+        fitting values. Shape is (ntemplates,).
     """
 
     def __init__(self, hdata, templates):
@@ -39,7 +59,7 @@ class PoissonNLL:
             A 2D array of shape (n_templates, n_bins)
         """
         values = self._templates.values
-        return values/np.sum(values, axis=1).reshape(-1, 1)
+        return values / np.sum(values, axis=1).reshape(-1, 1)
 
     @property
     def x0(self):
@@ -56,5 +76,6 @@ class PoissonNLL:
             The value of the negative log likelihood at `x`.
         """
         poi = x
-        exp_evts_per_bin = poi@self.fraction_matrix()
-        return np.sum(exp_evts_per_bin - self._data*np.log(exp_evts_per_bin))
+        exp_evts_per_bin = poi @ self.fraction_matrix()
+        return np.sum(exp_evts_per_bin - self._data * np.log(exp_evts_per_bin))
+
