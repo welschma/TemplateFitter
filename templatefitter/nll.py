03b6279078d56111f855a704a880f36a33389cf0
Maximilian Welsch <maximilian.welsch@student.kit.edu>
2019-01-10 17:22:49 +0100

    reworked negativeloglikelihood

--- templatefitter/nll.py
+++ templatefitter/nll.py
@@ -6,21 +6,45 @@ from abc import ABC, abstractmethod, abstractproperty
 
 import numpy as np
 
+from scipy.linalg import block_diag
 
-class AbstractNLL(ABC):
-    """Base class for all negative log likelihood functions.
+__all__ = [
+    "AbstractTemplateCostFunction",
+    "AdvancedPoissonNegativeLogLikelihood"
+]
+
+
+class AbstractTemplateCostFunction(ABC):
+    """Abstract base class for all cost function to estimate
+    yields using the template method.
+
+    Parameters
+    ----------
+    hdata : Histogram
+        Bin counts of the data histogram. Shape is (nbins,).
+    composite_template : Implementation of an AbstractCompositeTemplate
+        A CompositeTemplate instance. The templates are used to
+        extract the contribution from each process described by
+        the templates to the measured data set.
     """
 
-    @abstractproperty
+    def __init__(self, hdata, composite_template):
+        self._data = hdata
+        self._templates = composite_template
+
+    @property
     def x0(self):
-        pass
+        """numpy.ndarray: Starting values for the minimization."""
+        return self._templates.yield_values
+
+    # -- abstract methods --
 
     @abstractmethod
     def __call__(self):
         pass
 
 
-class PoissonNLL(AbstractNLL):
+class AdvancedPoissonNegativeLogLikelihood(AbstractTemplateCostFunction):
     """A negative log likelihood (NLL) function for binned data using
     template histograms shapes as pdfs. The NLL is calculated as
     :math:`-\log(L) = \sum\limits_i^{n_{\mathrm{bins}}} \\nu_i - n_i \log(\\nu_i)`,
@@ -46,24 +70,9 @@ class PoissonNLL(AbstractNLL):
         fitting values. Shape is (ntemplates,).
     """
 
-    def __init__(self, hdata, templates):
-        self._data = hdata
-        self._templates = templates
-
-    def fraction_matrix(self):
-        """Calculates the fractions of the templates in all bins.
-
-        Returns
-        -------
-        np.ndarray
-            A 2D array of shape (n_templates, n_bins)
-        """
-        values = self._templates.values
-        return values / np.sum(values, axis=1).reshape(-1, 1)
-
-    @property
-    def x0(self):
-        return self._templates.yields
+    def __init__(self, hdata, composite_template):
+        super().__init__(hdata, composite_template)
+        self._block_diag_inv_corr_mats = block_diag(*self._templates.inv_corr_mats)
 
     def __call__(self, x):
         """This function is called by the minimize method.
@@ -75,7 +84,20 @@ class PoissonNLL(AbstractNLL):
         float
             The value of the negative log likelihood at `x`.
         """
-        poi = x
-        exp_evts_per_bin = poi @ self.fraction_matrix()
-        return np.sum(exp_evts_per_bin - self._data.bin_counts * np.log(exp_evts_per_bin))
+        poi = x[:self._templates.num_templates]
+        nuiss_params = x[self._templates.num_templates:]
+
+        exp_evts_per_bin = poi @ self._templates.bin_fractions(
+                nuiss_params
+        )
+        poisson_term = np.sum(
+            exp_evts_per_bin - self._data.bin_counts * np.log(exp_evts_per_bin)
+                              )
+
+        gauss_term = 0.5*(nuiss_params@(
+                self._block_diag_inv_corr_mats@nuiss_params))
+
+        return poisson_term + gauss_term
+
+
 
