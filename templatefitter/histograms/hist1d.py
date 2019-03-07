b0bb30e0b8edfae4cb6c7e353be05de13c0cf386
Maximilian Welsch <maximilian.welsch@student.kit.edu>
2019-03-07 14:54:17 +0100

    reworked hist1d

--- templatefitter/histograms/hist1d.py
+++ templatefitter/histograms/hist1d.py
@@ -3,6 +3,7 @@ import numpy as np
 from scipy.stats import binned_statistic
 
 from templatefitter.histograms import AbstractHist
+from templatefitter.histograms import bin_mids, bin_widths, get_bin_range
 
 __all__ = [
     "Hist1d"
@@ -22,7 +23,6 @@ class Hist1d(AbstractHist):
         if data is not None:
             self.fill(data, weights)
 
-
     def _init_bin_edges(self, bins, range):
         if isinstance(bins, int):
             self._num_bins = bins
@@ -31,53 +31,73 @@ class Hist1d(AbstractHist):
         else:
             self._num_bins = len(bins) - 1
             self._bin_edges = bins
-            self._range = (np.amin(self._bin_edges), np.amax(self._bin_edges))
-
-    def _set_bin_counts(self, sample, bins, weights, range):
-
-        if weights is None:
-            weights = np.ones(len(sample))
-
-        self._bin_counts, self._bin_edges, _ = binned_statistic(
-            sample, weights, statistic='sum', bins=bins, range=range
-        )
-
-        self._bin_errors_sq, _, _ = binned_statistic(
-            sample, weights**2, statistic='sum', bins=bins, range=range
-        )
+            self._range = get_bin_range(self._bin_edges)
 
     def fill(self, data, weights):
-        pass
+        """Fills the histogram with given data and weights.
+
+        Parameters
+        ----------
+        data : numpy.ndarray, pandas.Series
+            Sequence which is filled into the histogram.
+        weights : numpy.ndarray, pandas.Series, optional
+            Sequence which is used as event weights. If no weights are
+            given, each event is weighted with 1.0. Default is None.
+        """
+        if weights is None:
+            weights = np.ones_like(data)
+        if isinstance(weights, list):
+            weights = np.array(weights)
+
+        self._bin_counts += binned_statistic(
+            data,
+            weights,
+            statistic='sum',
+            bins=self._bin_edges,
+            range=self._range
+        )[0]
+
+        self._bin_errors_sq += binned_statistic(
+            data,
+            weights ** 2,
+            statistic='sum',
+            bins=self._bin_edges,
+            range=self._range
+        )[0]
 
     @classmethod
-    def from_binned_data(cls, bin_counts, bin_edges):
-        pass
+    def from_binned_data(cls, bin_counts, bin_edges, bin_errors=None):
+        """Creates a `Hist1d` from a binned dataset.
 
-    @property
-    def bin_counts(self):
-        return self._bin_counts
+        Parameters
+        ----------
+        bin_edges : numpy.ndarray
+            Array with of bin edges.
+        bin_counts : numpy.ndarray
+            Array of bin counts.
+        bin_errors : numpy.ndarray
+            Array of bin errors.
 
-    @property
-    def bin_errors(self):
-        return np.sqrt(self._bin_errors_sq)
+        Returns
+        -------
+        histogram : Hist1d
+        """
+        instance = cls(bin_edges)
+        instance._bin_counts = bin_counts
 
-    @property
-    def bin_errors_sq(self):
-        return self._bin_errors_sq
+        if bin_errors is None:
+            bin_errors = np.sqrt(bin_counts)
 
-    @property
-    def bin_edges(self):
-        return self._bin_edges
+        instance._bin_errors_sq = bin_errors**2
+
+        return instance
+
 
     @property
     def bin_mids(self):
-        return (self.bin_edges[1:] + self.bin_edges[:-1]) / 2
+        return bin_mids(self.bin_edges)
 
     @property
     def bin_widths(self):
-        return self.bin_edges[1:] - self.bin_edges[:-1]
+        return bin_widths(self.bin_edges)
 
-    @property
-    def num_bins(self):
-        """int: Number of bins."""
-        return self._num_bins
--- tests/test_histograms/test_hist1d.py
+++ tests/test_histograms/test_hist1d.py
@@ -4,61 +4,235 @@ import numpy as np
 
 from templatefitter.histograms import Hist1d
 
-class TestHist1d(unittest.TestCase):
 
+class TestHist1d(unittest.TestCase):
     def setUp(self):
 
         self.iris_sepal_length = np.array(
-            [5.1, 4.9, 4.7, 4.6, 5. , 5.4, 4.6, 5. , 4.4, 4.9, 5.4, 4.8, 4.8,
-             4.3, 5.8, 5.7, 5.4, 5.1, 5.7, 5.1, 5.4, 5.1, 4.6, 5.1, 4.8, 5. ,
-             5. , 5.2, 5.2, 4.7, 4.8, 5.4, 5.2, 5.5, 4.9, 5. , 5.5, 4.9, 4.4,
-             5.1, 5. , 4.5, 4.4, 5. , 5.1, 4.8, 5.1, 4.6, 5.3, 5. , 7. , 6.4,
-             6.9, 5.5, 6.5, 5.7, 6.3, 4.9, 6.6, 5.2, 5. , 5.9, 6. , 6.1, 5.6,
-             6.7, 5.6, 5.8, 6.2, 5.6, 5.9, 6.1, 6.3, 6.1, 6.4, 6.6, 6.8, 6.7,
-             6. , 5.7, 5.5, 5.5, 5.8, 6. , 5.4, 6. , 6.7, 6.3, 5.6, 5.5, 5.5,
-             6.1, 5.8, 5. , 5.6, 5.7, 5.7, 6.2, 5.1, 5.7, 6.3, 5.8, 7.1, 6.3,
-             6.5, 7.6, 4.9, 7.3, 6.7, 7.2, 6.5, 6.4, 6.8, 5.7, 5.8, 6.4, 6.5,
-             7.7, 7.7, 6. , 6.9, 5.6, 7.7, 6.3, 6.7, 7.2, 6.2, 6.1, 6.4, 7.2,
-             7.4, 7.9, 6.4, 6.3, 6.1, 7.7, 6.3, 6.4, 6. , 6.9, 6.7, 6.9, 5.8,
-             6.8, 6.7, 6.7, 6.3, 6.5, 6.2, 5.9]
+            [
+                5.1,
+                4.9,
+                4.7,
+                4.6,
+                5.0,
+                5.4,
+                4.6,
+                5.0,
+                4.4,
+                4.9,
+                5.4,
+                4.8,
+                4.8,
+                4.3,
+                5.8,
+                5.7,
+                5.4,
+                5.1,
+                5.7,
+                5.1,
+                5.4,
+                5.1,
+                4.6,
+                5.1,
+                4.8,
+                5.0,
+                5.0,
+                5.2,
+                5.2,
+                4.7,
+                4.8,
+                5.4,
+                5.2,
+                5.5,
+                4.9,
+                5.0,
+                5.5,
+                4.9,
+                4.4,
+                5.1,
+                5.0,
+                4.5,
+                4.4,
+                5.0,
+                5.1,
+                4.8,
+                5.1,
+                4.6,
+                5.3,
+                5.0,
+                7.0,
+                6.4,
+                6.9,
+                5.5,
+                6.5,
+                5.7,
+                6.3,
+                4.9,
+                6.6,
+                5.2,
+                5.0,
+                5.9,
+                6.0,
+                6.1,
+                5.6,
+                6.7,
+                5.6,
+                5.8,
+                6.2,
+                5.6,
+                5.9,
+                6.1,
+                6.3,
+                6.1,
+                6.4,
+                6.6,
+                6.8,
+                6.7,
+                6.0,
+                5.7,
+                5.5,
+                5.5,
+                5.8,
+                6.0,
+                5.4,
+                6.0,
+                6.7,
+                6.3,
+                5.6,
+                5.5,
+                5.5,
+                6.1,
+                5.8,
+                5.0,
+                5.6,
+                5.7,
+                5.7,
+                6.2,
+                5.1,
+                5.7,
+                6.3,
+                5.8,
+                7.1,
+                6.3,
+                6.5,
+                7.6,
+                4.9,
+                7.3,
+                6.7,
+                7.2,
+                6.5,
+                6.4,
+                6.8,
+                5.7,
+                5.8,
+                6.4,
+                6.5,
+                7.7,
+                7.7,
+                6.0,
+                6.9,
+                5.6,
+                7.7,
+                6.3,
+                6.7,
+                7.2,
+                6.2,
+                6.1,
+                6.4,
+                7.2,
+                7.4,
+                7.9,
+                6.4,
+                6.3,
+                6.1,
+                7.7,
+                6.3,
+                6.4,
+                6.0,
+                6.9,
+                6.7,
+                6.9,
+                5.8,
+                6.8,
+                6.7,
+                6.7,
+                6.3,
+                6.5,
+                6.2,
+                5.9,
+            ]
         )
 
     def test_hist_from_int_bin(self):
         nbins = 10
-        iris_bc, iris_be = np.histogram(self.iris_sepal_length, bins=nbins)
-        iris_hist = Hist1d(self.iris_sepal_length,
-                           bins=nbins)
+        range = (0, 10)
+        iris_bc, iris_be = np.histogram(self.iris_sepal_length, bins=nbins, range=range)
+        iris_hist = Hist1d(data=self.iris_sepal_length, bins=nbins, range=range)
 
         np.testing.assert_array_equal(iris_hist.bin_counts, iris_bc)
         np.testing.assert_array_equal(iris_hist.bin_edges, iris_be)
 
+    def test_hist_bin_mids_widths(self):
+        nbins = 10
+        range = (0, 10)
+        iris_bc, iris_be = np.histogram(self.iris_sepal_length, bins=nbins, range=range)
+        iris_hist = Hist1d(data=self.iris_sepal_length, bins=nbins, range=range)
+
+        mids = (iris_be[:-1] + iris_be[1:]) / 2
+        widths = iris_be[1:] - iris_be[:-1]
+
+        np.testing.assert_array_equal(iris_hist.bin_mids, mids)
+        np.testing.assert_array_equal(iris_hist.bin_widths, widths)
+
     def test_hist_from_array_bins(self):
         bins = np.linspace(0, 10, 11)
         iris_bc, iris_be = np.histogram(self.iris_sepal_length, bins=bins)
-        iris_hist = Hist1d(self.iris_sepal_length,
-                           bins=bins)
+        iris_hist = Hist1d(data=self.iris_sepal_length, bins=bins)
 
         np.testing.assert_array_equal(iris_hist.bin_counts, iris_bc)
         np.testing.assert_array_equal(iris_hist.bin_edges, iris_be)
 
     def test_hist_with_range_option(self):
         nbins = 10
-        range=(4, 6)
-        iris_bc, iris_be = np.histogram(
-            self.iris_sepal_length, bins=nbins, range=range
-        )
-        iris_hist = Hist1d(
-            self.iris_sepal_length, bins=nbins, range=range
-        )
+        range = (4, 6)
+        iris_bc, iris_be = np.histogram(self.iris_sepal_length, bins=nbins, range=range)
+        iris_hist = Hist1d(data=self.iris_sepal_length, bins=nbins, range=range)
 
         np.testing.assert_array_equal(iris_hist.bin_counts, iris_bc)
         np.testing.assert_array_equal(iris_hist.bin_edges, iris_be)
+
+    def test_weighted_data(self):
+        bins = np.linspace(0, 10, 11)
+        weights = np.random.normal(1, 0.1, (len(self.iris_sepal_length)))
+        iris_bc, iris_be = np.histogram(
+            self.iris_sepal_length, weights=weights, bins=bins
+        )
+        iris_hist = Hist1d(data=self.iris_sepal_length, weights=weights, bins=bins)
+
+        np.testing.assert_array_almost_equal(iris_hist.bin_counts, iris_bc)
+        np.testing.assert_array_equal(iris_hist.bin_edges, iris_be)
+
+    def test_fill_data(self):
+        bins = np.linspace(0, 10, 11)
+        weights = np.random.normal(1, 0.1, (len(self.iris_sepal_length)))
+        iris_bc, iris_be = np.histogram(
+            self.iris_sepal_length, weights=weights, bins=bins
+        )
+        iris_hist = Hist1d(bins=bins)
+        iris_hist.fill(self.iris_sepal_length, weights)
+
+        np.testing.assert_array_almost_equal(iris_hist.bin_counts, iris_bc)
+        np.testing.assert_array_equal(iris_hist.bin_edges, iris_be)
+
+        iris_hist.fill(self.iris_sepal_length, weights)
+        np.testing.assert_array_almost_equal(iris_hist.bin_counts, 2 * iris_bc)
 
     def test_from_binned_data(self):
         bins = np.linspace(0, 10, 11)
         iris_bc, iris_be = np.histogram(self.iris_sepal_length, bins=bins)
 
-        iris_hist = Hist1d.from_binned_data(iris_bc, iris_be)
+        iris_hist = Hist1d.from_binned_data(bin_counts=iris_bc, bin_edges=iris_be)
 
         np.testing.assert_array_equal(iris_hist.bin_counts, iris_bc)
+        np.testing.assert_array_equal(iris_hist.bin_errors, np.sqrt(iris_bc))
         np.testing.assert_array_equal(iris_hist.bin_edges, iris_be)
