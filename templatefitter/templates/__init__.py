11ed4347a93b6d5d2faffbdb61337f622fe3cb05
Maximilian Welsch <maxwelsch93@gmail.com>
2019-03-09 15:37:03 +0100

    added channel to templates init

--- templatefitter/templates/__init__.py
+++ templatefitter/templates/__init__.py
@@ -2,3 +2,4 @@ from templatefitter.templates.abstract_template import *
 from templatefitter.templates.template1d import *
 from templatefitter.templates.template2d import *
 from templatefitter.templates.multi_channel_template import *
+from templatefitter.templates.channel import *
