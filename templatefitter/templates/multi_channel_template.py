8aca4f694a7a207ffde7a519c966a67560828e61
Maximilian Welsch <maxwelsch93@gmail.com>
2019-03-09 20:37:33 +0100

    defined interface for the multichanneltemplate

--- templatefitter/templates/multi_channel_template.py
+++ templatefitter/templates/multi_channel_template.py
@@ -9,4 +9,37 @@ __all__ = [
 
 
 class MultiChannelTemplate:
-    pass
+
+    def __init__(self):
+        pass
+
+    def define_channel(self):
+        pass
+
+    def define_channels(self):
+        pass
+
+    def define_process(self):
+        pass
+
+    def define_processes(self):
+        pass
+
+    def add_template(self):
+        pass
+
+    def add_templates(self):
+        pass
+
+    def add_data(self):
+        pass
+
+
+class NegLogLikelihood:
+
+    def __init__(self, multi_channel_template):
+        self._mcl = multi_channel_template
+
+    def __call__(self, x):
+        pass
+
