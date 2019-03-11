edb4a6ed71c54fe060ed60d36f9225b5c7ea0785
Maximilian Welsch <maximilian.welsch@student.kit.edu>
2019-03-11 18:41:09 +0100

    added a basic example to show the new interface

--- examples/basic_example.py
+++ examples/basic_example.py
@@ -1,54 +1,81 @@
+
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+import root_pandas
+
 import templatefitter as tf
 
-# Loading the DataFrames
-source_path = "/some/absolute/path"
-sig = pd.read_pickle(os.path.join(source_path, "PI0_signal.pkl"))
-remaining = pd.read_pickle(os.path.join(source_path, "PI0_remaining.pkl"))
-xulnu = pd.read_pickle(os.path.join(source_path, "PI0_xulnu.pkl"))
-data =  pd.read_pickle(os.path.join(source_path, "PI0_data.pkl"))
+ceph = "/ceph/welsch/moritz_data/"
 
-# Create the template
-nbins = 25
-limits = (-1.5, 3)
-st = tf.StackedTemplate(name="PI0_ALL", variable="missinMass", num_bins=nbins, limits=limits)
-# add templates to the templates
-# the order only matters in terms of the plotting order and the order how
-# parameters are handled internally
-st.create_template("xulnu", xulnu)
-st.create_template("remaining", remaining)
-st.create_template("signal", sig)
+signal = pd.read_pickle(ceph+"PI0_SIGNAL.pkl")
+remaining = pd.read_pickle(ceph+"PI0_remaining.pkl")
+xulnu = pd.read_pickle(ceph+"PI0_xulnu.pkl")
+data = pd.read_pickle(ceph+"DATA.pkl")
 
-# create a binned dataset
-data = tf.Hist1d(nbins, limits, data.missingMass)
-# data = st.generate_toy_dataset()
-# data = st.generate_asimov_dataset(integer_values=False)
+num_bins = 25
+limits = (-1.5, 5)
 
-# plot the pre fit distribution and the dataset
-# note: only the histograms are plotted, if you want to have a nicer plot
-# with axis titles, you have to set them yourself
-fig, ax = plt.subplots(1, 1, figsize=(8,8), dpi=200)
-st.plot_on(ax)
-ax.errorbar(data.bin_mids, data.bin_counts, yerr=data.bin_errors, ls="", marker=".", color="black")
+e_sig = signal.query("SignalChannel==1.")
+e_remaining = remaining.query("SignalChannel==1.")
+e_xulnu = xulnu.query("SignalChannel==1.")
+e_data = data.query("SignalChannel==1.")
 
-# perform the fit, you should get the fit result for the parameters as output in your terminal
-fitter = tf.TemplateFitter(data, st, "iminuit")
-fitter.do_fit(verbose=True)
+e_sig_eff = sum(e_sig.weight)/sum(signal.weight)
+e_remaining_eff = sum(e_remaining.weight)/sum(remaining.weight)
+e_xulnu_eff = sum(e_xulnu.weight)/sum(xulnu.weight)
 
-# plot the fit result
-fig, ax = plt.subplots(1, 1, figsize=(8,8), dpi=200)
-st.plot_on(ax)
-ax.errorbar(data.bin_mids, data.bin_counts, yerr=data.bin_errors, ls="", marker=".", color="black")
+print(e_sig_eff, e_remaining_eff, e_xulnu_eff)
 
-# create a profile likelihood plot for the signal yield parameter
-points, profile, hesse = fitter.profile("signal_yield")
-fig, ax = plt.subplots(1, 1, figsize=(8,8), dpi=200)
-ax.plot(points, profile, label="Profile NLL")
-ax.plot(points, hesse, label="Hesse approx.")
+e_hsig = tf.histograms.Hist1d(bins=num_bins, range=limits, data=e_sig.missingMass, weights=e_sig.weight)
+e_hremaining = tf.histograms.Hist1d(bins=num_bins, range=limits, data=e_remaining.missingMass,  weights=e_remaining.weight)
+e_hxulnu = tf.histograms.Hist1d(bins=num_bins, range=limits, data=e_xulnu.missingMass,  weights=e_xulnu.weight)
+e_tsig = tf.templates.Template1d("Signal", "missingMass", e_hsig)
+e_tremaining = tf.templates.Template1d("Remaining", "missingMass", e_hremaining)
+e_txulnu = tf.templates.Template1d("Signal", "missingMass", e_hxulnu)
 
-# create a toy study to perform linearity tests for yield paramters or for studying pull distributions
-toys = tf.ToyStudy(st, "iminuit")
-result = toys.do_linearity_test("signal", (0, 1000), n_points=3, n_exp=30)
-fig, ax = plt.subplots(1, 1, figsize=(8,8), dpi=200)
-ax.errorbar(x=result[0], y=result[1], yerr=result[2], ls="", marker=".")
+
+mu_sig = signal.query("SignalChannel==2.")
+mu_remaining = remaining.query("SignalChannel==2.")
+mu_xulnu = xulnu.query("SignalChannel==2.")
+mu_data = data.query("SignalChannel==2.")
+
+mu_sig_eff = sum(mu_sig.weight)/sum(signal.weight)
+mu_remaining_eff = sum(mu_remaining.weight)/sum(remaining.weight)
+mu_xulnu_eff = sum(mu_xulnu.weight)/sum(xulnu.weight)
+print(mu_sig_eff, mu_remaining_eff, mu_xulnu_eff)
+
+mu_hsig = tf.histograms.Hist1d(bins=num_bins, range=limits, data=mu_sig.missingMass, weights=mu_sig.weight)
+mu_hremaining = tf.histograms.Hist1d(bins=num_bins, range=limits, data=mu_remaining.missingMass, weights=mu_remaining.weight)
+mu_hxulnu = tf.histograms.Hist1d(bins=num_bins, range=limits, data=mu_xulnu.missingMass, weights=mu_xulnu.weight)
+
+mu_tsig = tf.templates.Template1d("Signal", "missingMass", mu_hsig)
+mu_tremaining = tf.templates.Template1d("Remaining", "missingMass", mu_hremaining)
+mu_txulnu = tf.templates.Template1d("Signal", "missingMass", mu_hxulnu)
+
+
+mct = tf.templates.MultiChannelTemplate()
+
+mct.define_channel("e", num_bins, limits)
+mct.define_channel("mu", num_bins, limits)
+mct.define_process("signal")
+mct.define_process("remaining")
+mct.define_process("xulnu")
+
+mct.add_template("e", "signal", e_tsig, e_sig_eff)
+mct.add_template("e", "remaining", e_tremaining, e_remaining_eff)
+mct.add_template("e", "xulnu", e_txulnu, e_xulnu_eff)
+
+mct.add_template("mu", "signal", mu_tsig, mu_sig_eff)
+mct.add_template("mu", "remaining", mu_tremaining,mu_remaining_eff)
+mct.add_template("mu", "xulnu", mu_txulnu, mu_xulnu_eff)
+
+mct.add_data(e=tf.histograms.Hist1d(num_bins, limits, data=e_data.missingMass))
+mct.add_data(mu=tf.histograms.Hist1d(num_bins, limits, data=mu_data.missingMass))
+
+nll = mct.create_nll()
+nll(nll.x0)
+
+# fitter = tf.TemplateFitter(mct, "iminuit")
+# fitter._nll.x0
+# fitter.do_fit(fix_nui_params=True, update_templates=False)
