5b74a72de16bb9ee279c59f5db943123b0eed415
Maximilian Welsch <maximilian.welsch@student.kit.edu>
2019-03-11 09:51:31 +0100

    fixed indentation

--- tests/test_templates/test_template1d.py
+++ tests/test_templates/test_template1d.py
@@ -10,25 +10,25 @@ from templatefitter.utility import get_systematic_cov_mat
 class TestHist1d(unittest.TestCase):
 
     def setUp(self):
-         self.data = np.array(
-             [5.1, 4.9, 4.7, 4.6, 5. , 5.4, 4.6, 5. , 4.4, 4.9, 5.4, 4.8, 4.8,
-              4.3, 5.8, 5.7, 5.4, 5.1, 5.7, 5.1, 5.4, 5.1, 4.6, 5.1, 4.8, 5. ,
-              5. , 5.2, 5.2, 4.7, 4.8, 5.4, 5.2, 5.5, 4.9, 5. , 5.5, 4.9, 4.4,
-              5.1, 5. , 4.5, 4.4, 5. , 5.1, 4.8, 5.1, 4.6, 5.3, 5. , 7. , 6.4,
-              6.9, 5.5, 6.5, 5.7, 6.3, 4.9, 6.6, 5.2, 5. , 5.9, 6. , 6.1, 5.6,
-              6.7, 5.6, 5.8, 6.2, 5.6, 5.9, 6.1, 6.3, 6.1, 6.4, 6.6, 6.8, 6.7,
-              6. , 5.7, 5.5, 5.5, 5.8, 6. , 5.4, 6. , 6.7, 6.3, 5.6, 5.5, 5.5,
-              6.1, 5.8, 5. , 5.6, 5.7, 5.7, 6.2, 5.1, 5.7, 6.3, 5.8, 7.1, 6.3,
-              6.5, 7.6, 4.9, 7.3, 6.7, 7.2, 6.5, 6.4, 6.8, 5.7, 5.8, 6.4, 6.5,
-              7.7, 7.7, 6. , 6.9, 5.6, 7.7, 6.3, 6.7, 7.2, 6.2, 6.1, 6.4, 7.2,
-              7.4, 7.9, 6.4, 6.3, 6.1, 7.7, 6.3, 6.4, 6. , 6.9, 6.7, 6.9, 5.8,
-              6.8, 6.7, 6.7, 6.3, 6.5, 6.2, 5.9]
-         )
+        self.data = np.array(
+            [5.1, 4.9, 4.7, 4.6, 5. , 5.4, 4.6, 5. , 4.4, 4.9, 5.4, 4.8, 4.8,
+             4.3, 5.8, 5.7, 5.4, 5.1, 5.7, 5.1, 5.4, 5.1, 4.6, 5.1, 4.8, 5. ,
+             5. , 5.2, 5.2, 4.7, 4.8, 5.4, 5.2, 5.5, 4.9, 5. , 5.5, 4.9, 4.4,
+             5.1, 5. , 4.5, 4.4, 5. , 5.1, 4.8, 5.1, 4.6, 5.3, 5. , 7. , 6.4,
+             6.9, 5.5, 6.5, 5.7, 6.3, 4.9, 6.6, 5.2, 5. , 5.9, 6. , 6.1, 5.6,
+             6.7, 5.6, 5.8, 6.2, 5.6, 5.9, 6.1, 6.3, 6.1, 6.4, 6.6, 6.8, 6.7,
+             6. , 5.7, 5.5, 5.5, 5.8, 6. , 5.4, 6. , 6.7, 6.3, 5.6, 5.5, 5.5,
+             6.1, 5.8, 5. , 5.6, 5.7, 5.7, 6.2, 5.1, 5.7, 6.3, 5.8, 7.1, 6.3,
+             6.5, 7.6, 4.9, 7.3, 6.7, 7.2, 6.5, 6.4, 6.8, 5.7, 5.8, 6.4, 6.5,
+             7.7, 7.7, 6. , 6.9, 5.6, 7.7, 6.3, 6.7, 7.2, 6.2, 6.1, 6.4, 7.2,
+             7.4, 7.9, 6.4, 6.3, 6.1, 7.7, 6.3, 6.4, 6. , 6.9, 6.7, 6.9, 5.8,
+             6.8, 6.7, 6.7, 6.3, 6.5, 6.2, 5.9]
+        )
 
-         self.bins = 10
-         self.range = (2, 7)
-         self.hist = Hist1d(bins=self.bins, range=self.range, data=self.data)
-         self.template = Template1d("test", "test_var", self.hist)
+        self.bins = 10
+        self.range = (2, 7)
+        self.hist = Hist1d(bins=self.bins, range=self.range, data=self.data)
+        self.template = Template1d("test", "test_var", self.hist)
 
     def test_empty_template(self):
         hist = Hist1d(bins=self.bins, range=self.range)
@@ -37,7 +37,6 @@ class TestHist1d(unittest.TestCase):
         np.testing.assert_array_equal(template.values, hist.bin_counts)
         np.testing.assert_array_equal(template.errors, np.zeros(self.bins))
         np.testing.assert_array_equal(template.fractions(np.zeros(self.bins)), np.zeros(self.bins))
-
 
     def test_values(self):
         np.testing.assert_array_almost_equal(self.template.values, self.hist.bin_counts)
