9bb718e375cea8554ee8b5139ace7704389b1a7e
Maximilian Welsch <maximilian.welsch@student.kit.edu>
2019-03-13 15:42:22 +0100

    move toy study to its own module

--- templatefitter/__init__.py
+++ templatefitter/__init__.py
@@ -1,5 +1,6 @@
 from templatefitter.nll import *
 from templatefitter.fitter import *
+from templatefitter.toy_study import *
 from templatefitter.minimizer import *
 from templatefitter.utility import *
 from templatefitter.stats import *
